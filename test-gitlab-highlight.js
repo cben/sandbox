c0b7d57907533abfbdb675dc3373aa60b348a486
Beni Cherniavsky-Paskin <beni.cherniavsky@gmail.com>
2019-04-01 10:55:57 +0000

    Update test-gitlab-highlight.js

--- test-gitlab-highlight.js
+++ test-gitlab-highlight.js
@@ -1,4 +1,8 @@
 // problem seen on private gitlab Community Edition 10.8.7 eb600b0
+// does it still happen on gitlab.com?  yep, Enterprise Edition 11.9.4-ee
+
+// the \\ escapes itself, not the following ', so the `splitToken(token)` line
+// should be colored as code, not string.
   
 const foo = [
     'TOKEN="\\',
